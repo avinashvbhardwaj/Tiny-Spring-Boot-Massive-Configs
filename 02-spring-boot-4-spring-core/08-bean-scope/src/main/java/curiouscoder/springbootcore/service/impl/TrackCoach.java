d9a9a89838db521e2c77a8549c1456bc199e09c9
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-04-25 20:33:50 +0530

    bean scope

---
+++ 02-spring-boot-4-spring-core/08-bean-scope/src/main/java/curiouscoder/springbootcore/service/impl/TrackCoach.java
@@ -0,0 +1,21 @@
+package curiouscoder.springbootcore.service.impl;
+
+import curiouscoder.springbootcore.service.Coach;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("TrackCoach: no-arg constructor");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a Hard 5K!!!";
+    }
+
+}
