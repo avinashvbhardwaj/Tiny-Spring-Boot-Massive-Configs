aa2f404f729046f35241127af87f136bf4977675
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-04-25 23:50:12 +0530

    delete by entity manager

---
+++ 03-spring-boot-4-hibernate-jpa-crud/06-crud-delete-student/src/main/java/curiouscoder/springbootcrud/dao/StudentDAO.java
@@ -0,0 +1,21 @@
+package curiouscoder.springbootcrud.dao;
+
+import curiouscoder.springbootcrud.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student entity);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+}
---
+++ 03-spring-boot-4-hibernate-jpa-crud/06-crud-delete-student/src/main/java/curiouscoder/springbootcrud/dao/impl/StudentDAOImpl.java
@@ -0,0 +1,78 @@
+package curiouscoder.springbootcrud.dao.impl;
+
+import curiouscoder.springbootcrud.dao.StudentDAO;
+import curiouscoder.springbootcrud.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    //define fields for entity manager
+    private final EntityManager entityManager;
+
+    // inject entity manager using constructor
+    // only one construction no need for autowired
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    //implement ave method
+    @Override
+    @Transactional
+    public void save(Student entity) {
+        entityManager.persist(entity);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll(){
+        //create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName, firstName desc", Student.class);
+
+        //return a query result
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        //create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:lastName", Student.class);
+        theQuery.setParameter("lastName", lastName);
+        //return a query result
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        Student updatedStudent = entityManager.merge(theStudent);
+        System.out.println("updatedStudent: " + updatedStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        System.out.println("deleting id " + id);
+        // retrieve the student
+        Student theStudent = entityManager.find(Student.class, id);
+
+        //display the student
+        System.out.println(theStudent);
+
+        //delete the student
+        entityManager.remove(theStudent);
+        System.out.println("removed id " + id);
+    }
+
+}
