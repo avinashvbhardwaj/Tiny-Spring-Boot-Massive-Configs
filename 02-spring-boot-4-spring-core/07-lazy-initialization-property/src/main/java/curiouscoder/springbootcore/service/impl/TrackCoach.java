49081af9964ca87dec133857727859f4a5aca60d
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-04-25 20:10:35 +0530

    lazy initialization using property

---
+++ 02-spring-boot-4-spring-core/07-lazy-initialization-property/src/main/java/curiouscoder/springbootcore/service/impl/TrackCoach.java
@@ -0,0 +1,18 @@
+package curiouscoder.springbootcore.service.impl;
+
+import curiouscoder.springbootcore.service.Coach;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("TrackCoach: no-arg constructor");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a Hard 5K!!!";
+    }
+
+}
