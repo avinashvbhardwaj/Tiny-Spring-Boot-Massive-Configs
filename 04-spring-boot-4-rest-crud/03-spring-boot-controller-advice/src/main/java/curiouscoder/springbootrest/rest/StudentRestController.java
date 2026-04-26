74432cd9f88558d0a0f37769d160f6ad8056aadd
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-04-26 14:41:03 +0530

    Controller Advice

---
+++ 04-spring-boot-4-rest-crud/03-spring-boot-controller-advice/src/main/java/curiouscoder/springbootrest/rest/StudentRestController.java
@@ -0,0 +1,45 @@
+package curiouscoder.springbootrest.rest;
+
+import curiouscoder.springbootrest.entity.Student;
+import curiouscoder.springbootrest.entity.StudentNotFoundException;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Avinash", "Bhardwaj"));
+        theStudents.add(new Student("Shubham", "Tiwari"));
+        theStudents.add(new Student("Mahesh", "Hanwatkar"));
+
+    }
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable Integer studentId) {
+
+        if (studentId >= theStudents.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return theStudents.get(studentId);
+    }
+
+}
