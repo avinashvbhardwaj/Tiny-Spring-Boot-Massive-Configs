dcaf69256ea50bb01050c408bd9887da8cb57dc5
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-04-26 12:48:15 +0530

    Path variable

--- 04-spring-boot-4-rest-crud/01-spring-boot-rest-crud/src/main/java/curiouscoder/springbootrest/rest/StudentRestController.java
+++ 04-spring-boot-4-rest-crud/01-spring-boot-rest-crud/src/main/java/curiouscoder/springbootrest/rest/StudentRestController.java
@@ -2,9 +2,10 @@ package curiouscoder.springbootrest.rest;
 
 import curiouscoder.springbootrest.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +16,23 @@ public class StudentRestController {
 
     private List<Student> theStudents;
 
-    // define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Avinash", "Bhardwaj"));
         theStudents.add(new Student("Shubham", "Tiwari"));
         theStudents.add(new Student("Mahesh", "Hanwatkar"));
 
+    }
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable Integer studentId) {
+        return theStudents.get(studentId);
     }
 }
