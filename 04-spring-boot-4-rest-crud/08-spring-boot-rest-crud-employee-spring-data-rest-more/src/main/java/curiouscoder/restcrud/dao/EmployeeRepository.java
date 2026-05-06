3c7249ceeae8f4e86f44cf6006a2dedb569ab8e7
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-05-07 00:04:03 +0530

    spring-data-rest:pagination,sort

---
+++ 04-spring-boot-4-rest-crud/08-spring-boot-rest-crud-employee-spring-data-rest-more/src/main/java/curiouscoder/restcrud/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package curiouscoder.restcrud.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import curiouscoder.restcrud.model.Employee;
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    // that's it no need to write any code!!!
+}
