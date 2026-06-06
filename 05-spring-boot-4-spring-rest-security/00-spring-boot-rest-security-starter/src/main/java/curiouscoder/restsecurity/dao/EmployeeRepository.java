69594e26e17d0e3b33435197580df987ed3f9339
Avinash Bhardwaj <avinashvbhardwaj@gmail.com>
2026-06-06 23:10:41 +0530

    SecurityFilterChain

--- 05-spring-boot-4-spring-rest-security/00-spring-boot-rest-security-starter/src/main/java/curiouscoder/restsecurity/dao/EmployeeRepository.java
+++ 05-spring-boot-4-spring-rest-security/00-spring-boot-rest-security-starter/src/main/java/curiouscoder/restsecurity/dao/EmployeeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import curiouscoder.restsecurity.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    // that's it no need to write any code!!!
+    // that's it, no need to write any code!!!
 }
